8da6b5045bac3403afab4a683fecb393ec92eb0c
gaetan <hadjeres.g@gmail.com>
2017-07-27 16:25:18 +0200

    add kendall_tauu

--- deepPermutations/permutation_distance.py
+++ deepPermutations/permutation_distance.py
@@ -1,13 +1,22 @@
 import numpy as np
-from scipy.stats import rankdata
+from scipy.stats import rankdata, kendalltau
 
 
-def spearman_rho(v1, v2):
+def distance_from_name(distance_name: str):
+    if distance_name == 'spearman':
+        return spearman_rho
+    elif distance_name == 'kendall':
+        return kendall_tau
+
+
+def spearman_rho(v1, v2, l=None):
     assert len(v1.shape) == 1
     assert len(v1) == len(v2)
-    l = len(v1)
-    # l = 128
-    l = 256
+    if l is None:
+        l = len(v1)
+        l = 128
+        # l = 256
+
     # apply function
     f = lambda x: x
     # f = lambda x: np.power(x, 1/2)
@@ -27,6 +36,30 @@ def spearman_rho(v1, v2):
     return np.sqrt(np.sum(np.square(r1 - r2)))
 
 
-def permutation_distance(input_1, input_2, distance_model):
-    return spearman_rho(distance_model.predict(input_1, batch_size=1)[0],
-                        distance_model.predict(input_2, batch_size=2)[0])
+def kendall_tau(v1, v2, l=None):
+    assert len(v1.shape) == 1
+    assert len(v1) == len(v2)
+    if l is None:
+        l = len(v1)
+        l = 128
+        # l = 256
+
+    # apply function
+    f = lambda x: x
+    # f = lambda x: np.power(x, 1/2)
+    squash = np.vectorize(lambda rk: f(l) if rk > l else f(rk))
+
+    # compute ranks
+    r1 = squash(rankdata(-v1, method='average'))
+    r2 = squash(rankdata(-v2, method='average'))
+
+    # print number of zeros
+    print('Kendall:')
+    print(sum(v1 > 1), sum(v2 > 1))
+    print(sum(v1 > 0), sum(v2 > 0))
+    print(sum(v1 == 0), sum(v2 == 0))
+    print(sum(v1 < 0), sum(v2 < 0))
+
+    tau, p_value = kendalltau(r1, r2)
+    # tau = 1 means correlation
+    return -tau
